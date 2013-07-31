40a0e5405911011ffc74a3f2be104e15f1f164a6
アーキテクトチーム <submit@inte.co.jp>
2013-07-31 16:02:22 +0900

    first commit

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_gantt_with_date do
+  name 'Redmine Gantt With Date plugin'
+  author 'y.yoshida'
+  description 'replace cweek with day on the gannt chart.'
+  version '0.0.1'
+  url 'https://github.com/yoshidayo/redmine_gantt_with_date'
+  author_url 'http://www.ibs.inte.co.jp/'
+end
